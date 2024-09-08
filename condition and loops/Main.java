c4c0999724ea2ba93ebb425368a4a56f46d0e201
Abir-Shaikh <shaikhabir044@gmail.com>
2024-09-08 20:47:27 +0530

    first commit

---
+++ condition and loops/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int salary = 38399;
+//        if (salary > 20000) {
+//            salary = salary+2000;
+//        }else{
+//            salary = salary+1000;
+//        }
+
+        //multiple line if else
+        //if
+        //elseif
+        //else
+
+    }
+}
