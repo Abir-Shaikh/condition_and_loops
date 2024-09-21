90d4c719f2152e8531533081b31285867da9c549
Abir-Shaikh <shaikhabir044@gmail.com>
2024-09-21 22:39:17 +0530

    weekdaySwitch Commit

---
+++ condition and loops/weekdaySwitch.java
@@ -0,0 +1,37 @@
+//Write a program to read a weekday number and print weekday name using switch statement
+
+import java.util.Scanner;
+public class weekdaySwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the day of the week");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
